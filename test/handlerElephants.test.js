32686fc52f18ad3964731d068065532fa0d4b097
Talita Saez <tltsaez@gmail.com>
2022-08-01 12:37:14 -0300

    resolução desafio 15 bonus

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -6,11 +6,11 @@ describe('Testes da função HandlerElephants', () => {
   });
 
   it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson;', () => {
-    expect(handlerElephants('names')).toBe(['Jefferson', 'Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   });
 
   it('Para o argumento averageAge deve retornar um número próximo a 10.5;', () => {
-    expect(handlerElephants('averageAge')).toEqua(10.5);
+    expect(handlerElephants('averageAge')).toEqual(10.5);
   });
 
   it('Não passando argumentos a função deve retornar undefined', () => {
@@ -22,7 +22,10 @@ describe('Testes da função HandlerElephants', () => {
   });
 
   it('', () => {
-    expect();
+    expect(handlerElephants('location')).toEqual(('NW'));
+  });
+  it('', () => {
+    expect(handlerElephants({})).toEqual(('Parâmetro inválido, é necessário uma string'));
   });
 });
 
