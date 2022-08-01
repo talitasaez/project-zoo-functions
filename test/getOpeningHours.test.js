106c95eaaa092d4c152f299fc805475a4e310b71
Talita Saez <tltsaez@gmail.com>
2022-08-01 14:34:26 -0300

    resolução desafio 13 e 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -20,4 +20,42 @@ describe('Testes da função getOpeningHours', () => {
   it('', () => {
     expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual(('The zoo is open'));
   });
+
+  it('', () => {
+    expect(typeof getOpeningHours).toEqual('function');
+  });
+
+  it('', () => {
+    expect(() => {
+      getOpeningHours('Monday', '13:00-AM');
+    }).toThrow('The hour must be between 0 and 12');
+  });
+
+  it('', () => {
+    expect(() => {
+      getOpeningHours('Thu', '13:00-AM');
+    }).toThrow('The day must be valid. Example: Monday');
+  });
+
+  it('', () => {
+    expect(() => {
+      getOpeningHours('Friday', '09:00-ZM');
+    }).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+
+  it('', () => {
+    expect(() => {
+      getOpeningHours('Saturday', 'C9:00-AM');
+    }).toThrow('The hour should represent a number');
+  });
+  it('', () => {
+    expect(() => {
+      getOpeningHours('Sunday', '09:c0-AM');
+    }).toThrow('The minutes should represent a number');
+  });
+  it('', () => {
+    expect(() => {
+      getOpeningHours('Tuesday', '09:60-AM');
+    }).toThrow('The minutes must be between 0 and 59');
+  });
 });
