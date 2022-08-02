07454e42474872c57e589bca4595bbef51bc7b15
Talita Saez <tltsaez@gmail.com>
2022-08-02 19:24:24 -0300

    resolução desafio 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+function getEmployeesCoverage(id) {
   // seu código aqui
+  const ids = employees.find((element) => element.id);
+  const nome = employees.find((element) => element.firstName);
+  const sobrenome = employees.find((element) => element.lastName);
+  const nomes = nome.concat(sobrenome);
+  const nomeAnimal = species.find((element) => element.name);
+  const especie = employees.find((element) => element.responsibleFor.includes(nomeAnimal));
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((element) => element.id === id);
+  const firstSpecies = employee.responsibleFor.find((element) => element[0]);
+  const findResidents = data.species.find((element) => element.id === firstSpecies).residents;
+  const ages = findResidents.map((element) => element.age);
+  const sortAges = ages.sort((a, b) => a - b);
+  const oldest = findResidents.find((element) => element.age === sortAges[sortAges.length - 1]);
+  const resultado = Object.values(oldest);
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
