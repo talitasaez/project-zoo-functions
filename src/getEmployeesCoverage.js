58bb794dc04b096dcfa283e0dd7c599ddacb4f4d
Talita Saez <tltsaez@gmail.com>
2022-08-04 12:13:21 -0300

    resolução desafio 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,5 +1,31 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage(id) {}
+function formatacao(parametro) {
+  const empl = employees.find((element) => (
+    element.id === parametro.id) || (element.firstName === parametro.name)
+  || (element.lastName === parametro.name));
+  if (empl === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const especies = empl.responsibleFor.map((element) => species.find((el) => el.id === element));
+  const especiesNome = especies.map((element) => element.name);
+  const localizacao = especies.map((element) => element.location);
+  const resultado = {
+    id: empl.id,
+    fullName: `${empl.firstName} ${empl.lastName}`,
+    species: especiesNome,
+    locations: localizacao,
+  };
+  return resultado;
+}
+function getEmployeesCoverage(parametro) {
+  const resultadoTotal = employees.map((element) => formatacao(element));
+  if (parametro === undefined) {
+    return resultadoTotal;
+  } if (parametro.id || parametro.name) {
+    return formatacao(parametro);
+  }
+}
 
 module.exports = getEmployeesCoverage;
